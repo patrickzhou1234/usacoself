4e872a8dac900c0d3cdd0457d9b072cbcc1e84ef
patrickzhou1234 <patrickchang44543@gmail.com>
2022-11-12 16:45:29 -0800

    measuringtraffic

--- measuringtraffic/main.cpp
+++ measuringtraffic/main.cpp
@@ -12,39 +12,47 @@ struct segment {
 int main() {
     freopen("traffic.in", "r", stdin);
     freopen("traffic.out", "w", stdout);
-    int n, i, ansct=0, mncur=MAX_INT, mxcur=-MAX_INT, mxrec=-MAX_INT, mnrec=MAX_INT, mnexit=MAX_INT, mxexit=-MAX_INT;
+    int n, i;
     cin >> n;
     string arr[n];
     segment segs[n];
     for (i=0;i<n;i++) {
         cin >> arr[i] >> segs[i].a >> segs[i].b;
     }
+    int maxR=1000000, minR=0;
     for (i=n-1;i>=0;i--) {
+        if (arr[i]=="off") {
+            minR+=segs[i].a;
+            maxR+=segs[i].b;
+        }
         if (arr[i]=="on") {
-            if (i!=n-1 && arr[i+1]!="on") {
-                mxrec = -MAX_INT;
-                mnrec = MAX_INT;
-            }
-            mxrec = max(segs[i].a, mxrec);
-            mnrec = min(segs[i].b, mnrec);
+            minR-=segs[i].b;
+            minR = max(0, minR);
+            maxR-=segs[i].a;
+            maxR = max(0, maxR);
         }
         if (arr[i]=="none") {
-            if (i!=n-1 && arr[i+1]!="none") {
-                mxcur = -MAX_INT;
-                mncur = MAX_INT;
-            }
-            mxcur = max(mxcur, segs[i].a);
-            mncur = min(mncur, segs[i].b);
-        }
-        if (arr[i]=="off") {
-            if (i!=n-1 && arr[i+1]!="off") {
-                mxexit = -MAX_INT;
-                mnexit = MAX_INT;
-            }
-            mxexit = max(segs[i].a, mxexit);
-            mnexit = min(segs[i].b, mnexit);
+            minR = max(minR, segs[i].a);
+            maxR = min(maxR, segs[i].b);
         }
     }
-    cout << max(mxcur-mnrec, 0) << " " << max(mncur-mxrec, 0) << endl << max(mxcur-mnexit, 0) << " " << max(mncur-mxexit, 0);
+    cout << minR << " " << maxR << "\n";
+    for (i=0;i<n;i++) {
+        if (arr[i]=="on") {
+            minR+=segs[i].a;
+            maxR+=segs[i].b;
+        }
+        if (arr[i]=="off") {
+            minR-=segs[i].b;
+            minR = max(0, minR);
+            maxR-=segs[i].a;
+            maxR = max(0, maxR);
+        }
+        if (arr[i]=="none") {
+            minR = max(minR, segs[i].a);
+            maxR = min(maxR, segs[i].b);
+        }
+    }
+    cout << minR << " " << maxR;
     return 0;
 }

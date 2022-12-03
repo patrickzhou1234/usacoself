f9e82f3ebd800be8551125680e96d2e69a11db61
patrickzhou1234 <patrickchang44543@gmail.com>
2022-12-02 21:15:40 -0800

    nicebro

--- oddphotos/main.cpp
+++ oddphotos/main.cpp
@@ -7,69 +7,24 @@ using namespace std;
 #define MAX_INT 2147483647
 
 int main() {
-    int n, i, j, even=0, odd=0, ct=0, tmpval;
-    bool ev=true;
+    int n, i, inp, even=0, odd=0;
     cin >> n;
-    vector<int> a;
     for (i=0;i<n;i++) {
-        cin >> tmpval;
-        a.push_back(tmpval);
-    }
-    for (i=0;i<n;i++) {
-        if (a[i]%2==0) {
+        cin >> inp;
+        if (inp%2==0) {
             even++;
         } else {
             odd++;
         }
     }
-    even=0;
-    odd=0;
-    for (i=0;i<n;i++) {
-        for (j=0;j<n;j++) {
-            if (i%2==0 && j%2==0 && ev) {
-                even++;
-                ev=false;
-                if (j>=i) {
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                } else {
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                }
-            } else if (i%2!=0 && j%2!=0 && ev) {
-                even++;
-                ev = false;
-                if (j>=i) {
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                } else {
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                }
-            } else if (i%2!=0 && j%2==0 && !ev) {
-                odd++;
-                ev = true;
-                if (j>=i) {
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                } else {
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                }
-            } else if (i%2==0 && j%2!=0 && !ev) {
-                odd++;
-                ev = true;
-                if (j>=i) {
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                } else {
-                    a.erase(a.begin()+j, a.begin()+j+1);
-                    a.erase(a.begin()+i, a.begin()+i+1);
-                }
-            }
-        }
+    while(odd>even) {
+        odd-=2;
+        even++;
     }
-    cout << min(odd, even);
+    if (even>odd+1) {
+        even=odd+1;
+    }
+    cout << even+odd;
     system("pause");
     return 0;
 }

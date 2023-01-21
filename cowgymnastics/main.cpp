232c4e2e12878c02e9e500117b2ba76579e5533a
patrickzhou1234 <patrickchang44543@gmail.com>
2023-01-20 20:39:21 -0800

    halfsolved

---
+++ cowgymnastics/main.cpp
@@ -0,0 +1,28 @@
+// http://www.usaco.org/index.php?page=viewproblem2&cpid=963
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+int main() {
+    freopen("gymnastics.in", "r", stdin);
+    freopen("gymnastics.out", "w", stdout);
+    int k, n, i, j;
+    cin >> k >> n;
+    int mat[k][n];
+    for (i=0;i<k;i++) {
+        for (j=0;j<n;j++) {
+            cin >> mat[i][j];
+        }
+    }
+    bool 
+    for (i=0;i<k;i++) {
+        for (j=0;j<n;j++) {
+
+        }
+    }
+    system("pause");
+    return 0;
+}

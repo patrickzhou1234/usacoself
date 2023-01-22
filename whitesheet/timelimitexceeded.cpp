d8de55c542fca154c604e7b9a1f58845c3254a43
patrickzhou1234 <patrickchang44543@gmail.com>
2023-01-21 17:13:27 -0800

    tle

---
+++ whitesheet/timelimitexceeded.cpp
@@ -0,0 +1,39 @@
+// https://codeforces.com/contest/1216/problem/C
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+int main()
+{
+    int i, j;
+    int b1[4], b2[4], w[4];
+    for (i = 0; i < 4; i++)
+    {
+        cin >> w[i];
+    }
+    for (i = 0; i < 4; i++)
+    {
+        cin >> b1[i];
+    }
+    for (i = 0; i < 4; i++)
+    {
+        cin >> b2[i];
+    }
+    double d, e;
+    for (d = w[0] + 0.5; d < w[2]; d++)
+    {
+        for (e = w[1] + 0.5; e < w[3]; e++)
+        {
+            if (!((b1[0] <= d && d <= b1[2] && b1[1] <= e && e <= b1[3]) || (b2[0] <= d && d <= b2[2] && b2[1] <= e && e <= b2[3])))
+            {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+    cout << "NO";
+    system("pause");
+    return 0;
+}

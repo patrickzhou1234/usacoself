34c3ca0fff9cc1f2b4fdd89f17b819815edb2547
patrickzhou1234 <patrickchang44543@gmail.com>
2023-02-21 12:05:59 -0800

    otherfourcases

--- herdle/main.cpp
+++ herdle/main.cpp
@@ -6,37 +6,50 @@ using namespace std;
 
 #define MAX_INT 2147483647
 
-int main() {
+int main()
+{
     char a[3][3], b[3][3];
-    int i, j, m, cor=0, incor=0;
+    int i, j, m, cor = 0, incor = 0;
     bool checked[3][3] = {false};
-    for (i=0;i<3;i++) {
-        for (j=0;j<3;j++) {
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
             cin >> a[i][j];
         }
     }
-    for (i=0;i<3;i++) {
-        for (j=0;j<3;j++) {
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
             cin >> b[i][j];
         }
     }
-    for (i=0;i<3;i++) {
-        for (j=0;j<3;j++) {
-            if (a[i][j]==b[i][j] && checked[i][j]==false) {
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (a[j][i] == b[j][i] && checked[j][i] == false)
+            {
                 cor++;
-                checked[i][j] = true;
-            } else {
-                for (m=0;m<3;m++) {
-                    if (a[i][j]==b[i][m] && checked[i][m]==false) {
+                checked[j][i] = true;
+            }
+            else
+            {
+                for (m = 0; m < 3; m++)
+                {
+                    if (a[j][i] == b[m][j] && checked[m][j] == false)
+                    {
                         incor++;
-                        checked[i][m] = true;
+                        checked[m][j] = true;
                         break;
                     }
                 }
             }
         }
     }
-    cout << cor << "\n" << incor << endl;
+    cout << cor << "\n"
+         << incor << endl;
     system("pause");
     return 0;
 }
